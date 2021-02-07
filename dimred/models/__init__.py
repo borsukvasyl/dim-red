a83eaebb4380268920c76b16d0756df96fa29885
Vasyl Borsuk <borsukvasyl>
2021-02-07 14:31:04 +0200

    fixed imports

--- dimred/models/__init__.py
+++ dimred/models/__init__.py
@@ -1,6 +1,6 @@
-from .autoencoder import AutoEncoder
+from .autoencoder.autoencoder import AutoEncoder
 from .base_model import BaseModel
-from .kmeans import KMeans
-from .svd import SVDModel
+from .kmeans.kmeans import KMeans
+from .svd.svd import SVDModel
 
 __all__ = ["BaseModel", "AutoEncoder", "SVDModel", "KMeans"]
