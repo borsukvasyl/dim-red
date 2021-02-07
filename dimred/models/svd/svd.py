eaf0dcf4aa9ea05874b7959f27b3a5901ef24eec
Vasyl Borsuk <borsukvasyl>
2021-02-07 14:52:00 +0200

    added initial svd

--- dimred/models/svd/svd.py
+++ dimred/models/svd/svd.py
@@ -1,9 +1,31 @@
+from typing import List, Tuple
+
+import cv2
+import numpy as np
+
 from dimred.models import BaseModel
 
 
 class SVDModel(BaseModel):
-    def compress(self, img):
-        pass
+    def __init__(self, num_components: int):
+        self.num_components = num_components
 
-    def decompress(self, embedding):
-        pass
+    def compress(self, img: np.ndarray) -> List[Tuple[np.ndarray, np.ndarray, np.ndarray]]:
+        """
+        Compresses each image channel using SVD decomposition
+        """
+        result = []
+        for channel in cv2.split(img):
+            U, s, VT = np.linalg.svd(channel)
+            U, s, VT = U[:, :self.num_components], s[:self.num_components], VT[:self.num_components, :]
+            result.append((U, s, VT))
+        return result
+
+    def decompress(self, embedding: List[Tuple[np.ndarray, np.ndarray, np.ndarray]]) -> np.ndarray:
+        """
+        Decode SVD decomposition into original image
+        """
+        channels = [U @ np.diag(s) @ VT for U, s, VT in embedding]
+        img = cv2.merge(channels)
+        clipped_img = np.clip(img, 0, 255)
+        return clipped_img.astype(np.uint8)
