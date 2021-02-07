9256ac44cd588e0b8fde6dd59a8759e43813ca20
Vasyl Borsuk <borsukvasyl>
2021-02-07 15:04:21 +0200

    fixed imports and gitignore

--- dimred/models/__init__.py
+++ dimred/models/__init__.py
@@ -1,5 +1,5 @@
-from .autoencoder.autoencoder import AutoEncoder
 from .base_model import BaseModel
+from .autoencoder.autoencoder import AutoEncoder
 from .kmeans.kmeans import KMeans
 from .svd.svd import SVDModel
 
