f977b9a5c6298993f5e964e3b43435fae5d07715
Vasyl Borsuk <borsukvasyl>
2021-02-13 17:35:19 +0200

    added float16

--- dimred/models/svd/svd.py
+++ dimred/models/svd/svd.py
@@ -18,7 +18,8 @@ class SVDModel(BaseModel):
         for channel in cv2.split(img):
             U, s, VT = np.linalg.svd(channel)
             U, s, VT = U[:, :self.num_components], s[:self.num_components], VT[:self.num_components, :]
-            result.append((U.astype("float16"), s.astype("float16"), VT.astype("float16")))
+            U, s, VT = U.astype(np.float16), s, VT.astype(np.float16)
+            result.append((U, s, VT))
         return result
 
     def decompress(self, embedding: List[Tuple[np.ndarray, np.ndarray, np.ndarray]]) -> np.ndarray:
@@ -26,4 +27,6 @@ class SVDModel(BaseModel):
         Decode SVD decomposition into original image
         """
         channels = [U @ np.diag(s) @ VT for U, s, VT in embedding]
-        return np.array(channels).transpose((1, 2, 0)).clip(0, 255).astype("uint8")
+        img = np.dstack(channels)
+        clipped_img = np.clip(img, 0, 255)
+        return clipped_img.astype(np.uint8)
